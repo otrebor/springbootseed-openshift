a6a56e7f365958933d3583ca2474e2354b05f1a7
Roberto Bob <bob.dj@hotmail.it>
2018-11-01 21:11:16 +0100

    added servlet initializer

--- src/main/java/com/company/example/springbootseed/Application.java
+++ src/main/java/com/company/example/springbootseed/Application.java
@@ -5,19 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        Class[] sources = {Application.class, ServletInitializer.class};
+        Class[] sources = {Application.class};
         SpringApplication.run(sources, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        return application.sources(Application.class);
     }
 
     /* It retrieves all the beans that were created either by your app or were automatically added thanks to Spring Boot. It sorts them and prints them out. */
