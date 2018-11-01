5c2a66b09212a36463a9cb8532ec82c5b8ba956d
Roberto Bob <bob.dj@hotmail.it>
2018-11-01 23:40:28 +0100

    prove a caso

--- src/main/java/com/company/example/springbootseed/Application.java
+++ src/main/java/com/company/example/springbootseed/Application.java
@@ -4,15 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 
-@SpringBootApplication
+// @SpringBootApplication includes the following three annotations
+@Configuration /*tags the class as a source of bean definitions for the application context.*/
+@ComponentScan /*tells Spring to look for other components, configurations, and services in the this package, allowing it to find the controllers.*/
+@EnableAutoConfiguration /* tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings. */
 public class Application extends SpringBootServletInitializer {
 
     Logger logger = LoggerFactory.getLogger(Application.class);
